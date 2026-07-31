4425090ee031ad6c82a64244204769ff7d817545
Vansh-ui-hub <v0060960@gmail.com>
2026-07-31 13:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ if (user_input) and (leader_agent):
           code = response['messages'] [-1].content [-1] ['text']
           st.html(code, width="stretch", unsafe_allow_javascript=True)
       
-      except Exception as error:
-        st.error(err)
+          except Exception as error:
+            st.error(err)
 
 # TAB 3 Code
   with tab3:
